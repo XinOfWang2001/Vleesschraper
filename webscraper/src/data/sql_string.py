fe46e6cd253e7f7cf6388f40791de9c6bdd8a88b
XinOfWang2001 <Xin20Wang@outlook.com>
2026-07-14 00:27:54 +0200

    Reduced duplication

--- webscraper/src/data/sql_string.py
+++ webscraper/src/data/sql_string.py
@@ -1,10 +1,11 @@
 
-def get_filter_existing_meat_query() -> str:
-    with open("src/data/db/filter_existing_meat.sql", "r") as file:
+def _read_sql_file(file_path: str) -> str:
+    with open(file_path, "r") as file:
         query = file.read()
     return query
 
+def get_filter_existing_meat_query() -> str:
+    return _read_sql_file("src/data/db/filter_existing_meat.sql")
+
 def get_create_temp_table_query() -> str:
-    with open("src/data/db/create_temp_table.sql", "r") as file:
-        query = file.read()
-    return query
+    return _read_sql_file("src/data/db/create_temp_table.sql")
