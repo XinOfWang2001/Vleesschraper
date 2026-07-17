e71291f35d0e1cc8bf5e1fcf4e85acee1eaaad15
XinOfWang2001 <Xin20Wang@outlook.com>
2026-07-17 15:26:31 +0200

    Add Receiver method

---
+++ service_api/src/main/java/com/service_api/message_receiver/MeatpriceReceiver.java
@@ -0,0 +1,13 @@
+package com.service_api.message_receiver;
+
+/// @ Receiver class
+///  - Responsible for receiving messages from Kafka instance.
+///  - Just responsible for insertion of new data into Analytics database.
+///
+public class MeatpriceReceiver {
+    // @ Receive message from QUEUE, Butcher
+    // @ HERE Receiver method.
+
+    public MeatpriceReceiver() {}
+
+}
